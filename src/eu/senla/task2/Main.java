4f1547e1ec4736633aed670fd9f5b83ddab2b5f2
Роман Науменко <naumenkoroman.nik@gmail.com>
2021-01-24 15:46:48 +0300

    First commit

--- src/eu/senla/task2/Main.java
+++ src/eu/senla/task2/Main.java
@@ -1,7 +1,20 @@
 package eu.senla.task2;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Helloy");
+
+
+        Rainbow concreteObject = new Rainbow();
+        concreteObject.printColor(1);
+        concreteObject.printColor(2);
+        concreteObject.printColor(3);
+        concreteObject.printColor(4);
+        concreteObject.printColor(5);
+        concreteObject.printColor(6);
+        concreteObject.printColor(7);
+
+
     }
 }
--- src/eu/senla/task2/Rainbow.java
+++ src/eu/senla/task2/Rainbow.java
@@ -2,6 +2,42 @@ package eu.senla.task2;
 
 public class Rainbow {
 
-    private static final String Red = "1";
+    private static final int Red = 1;
+
+    private static final int Orange = 2;
+    private static final int Yellow = 3;
+    private static final int Green = 4;
+    private static final int Blue = 5;
+    private static final int Indigo = 6;
+    private static final int Violet = 7;
+
+
+    public void printColor(int rainbowColor) {
+
+        switch (rainbowColor) {
+            case Red:
+                System.out.println("Red");
+                break;
+            case Orange:
+                System.out.println("Orange");
+                break;
+            case Yellow:
+                System.out.println("Yellow");
+                break;
+            case Green:
+                System.out.println("Green");
+                break;
+            case Blue:
+                System.out.println("Blue");
+                break;
+            case Indigo:
+                System.out.println("Indigo");
+                break;
+            case Violet:
+                System.out.println("Violet");
+                break;
+
+        }
+    }
 
 }
