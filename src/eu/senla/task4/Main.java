73b81eae6ac181e051f0a98e8e1fa55e66524f5a
Роман Науменко <naumenkoroman.nik@gmail.com>
2021-01-31 19:19:55 +0300

    First commit

--- src/eu/senla/task4/Main.java
+++ src/eu/senla/task4/Main.java
@@ -4,19 +4,29 @@ public class Main {
     public static void main(String[] args) {
         String[][] array = new String[10][10];
         String letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+        String elem = " ";
         for (int i = 0; i < array.length; i++) {
-            for (int n = 0; n < array[0].length; n++) {
-                if (n % 3 == 0) array[i][n] = String.valueOf((double) Math.random() * 10 + 1).substring(0, 6);
-                else for (int x = 0; x < 6; x++) {
-                    array[i][n] = "";
-                    int l = (int) (Math.random() * 26);
-                    System.out.print(letter.charAt(l));
+            for (int j = 0; j < array[0].length; j++) {
+                if (j % 3 == 0 ) {
+                    array[i][j] = String.valueOf((double) Math.random() * 10 + 1).substring(0, 6);
+                } else {
+                    elem = " ";
+                    for (int x = 0; x < 6; x++) {
+                        int l = (int) (Math.random() * 26);
+                        elem = elem + letter.charAt(l);
+                    }
+                   array[i][j] = elem;
                 }
-                System.out.print(array[i][n] + " ");
+
+                System.out.print(array[i][j] + " ");
             }
             System.out.println();
+
         }
 
     }
 }
+
+
+
+
