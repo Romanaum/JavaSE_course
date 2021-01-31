c84f3cf3dce22b7275b1e43fbfaa6788cdf4babd
Роман Науменко <naumenkoroman.nik@gmail.com>
2021-01-31 19:26:34 +0300

    First commit

--- src/eu/senla/task4/Main.java
+++ src/eu/senla/task4/Main.java
@@ -7,7 +7,7 @@ public class Main {
         String elem = " ";
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[0].length; j++) {
-                if (j % 3 == 0 ) {
+                if (j % 3 == 0) {
                     array[i][j] = String.valueOf((double) Math.random() * 10 + 1).substring(0, 6);
                 } else {
                     elem = " ";
@@ -15,16 +15,14 @@ public class Main {
                         int l = (int) (Math.random() * 26);
                         elem = elem + letter.charAt(l);
                     }
-                   array[i][j] = elem;
+                    array[i][j] = elem;
                 }
-
                 System.out.print(array[i][j] + " ");
             }
             System.out.println();
-
         }
-
     }
+    
 }
 
 
